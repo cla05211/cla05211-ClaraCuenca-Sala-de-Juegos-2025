bf31aaf2880ded07b7dda7d8fecbaf3dee15b7ac
CLARA <CLARITA@DESKTOP-4J0NHPR>
2025-05-14 08:45:10 -0300

    Se arregla tamaño para pantallas mas grandes

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -40,7 +41,7 @@ export class HttpService
       )
     );
   }
-  
+
 
 }
 
